228ad6971bebae56976986c853da3ffc699c2f72
jarehec <129@holbertonschool.com>
2018-08-31 00:28:44 -0700

    new thought

--- 0x0C-heap_insert/1-heap_insert.c
+++ 0x0C-heap_insert/1-heap_insert.c
@@ -2,6 +2,47 @@
 
 
 /**
+ * heapify - bottom-up heap fixer
+ * @node: pointer to heap node
+ * Return: the new location of node
+*/
+heap_t *heapify(heap_t *node)
+{
+	int i;
+
+	if (!node || !node->parent)
+		return (NULL);
+	while (node->parent && node->n > node->parent->n)
+	{
+		i = node->parent->n;
+		node->parent->n = node->n;
+		node->n = i;
+		node = node->parent;
+	}
+	return (node);
+}
+
+/**
+ * binary_tree_balance - measures the balance of a binary tree
+ * @tree: binary tree
+ *
+ * Return: balance factor
+ */
+int binary_tree_balance(heap_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+			left = binary_tree_height(tree->left) + 1;
+		if (tree->right)
+			right = binary_tree_height(tree->right) + 1;
+	}
+	return (left - right);
+}
+
+/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: binary tree
  *
@@ -28,22 +69,24 @@ size_t binary_tree_height(heap_t *tree)
 }
 
 /**
- * is_balanced_heap - checks if the heap is balanced
- * @root: pointer to root node
- * Return: 1 if balanced otherwise 0
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: binary tree
+ *
+ * Return: number of nodes
  */
-int is_balanced_heap(heap_t *root)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	size_t count = 0;
 
-	if (root)
+	if (tree)
 	{
-		if (root->left)
-			left = binary_tree_height(root->left) + 1;
-		if (root->right)
-			right = binary_tree_height(root->right) + 1;
+		if (tree->left)
+			count += binary_tree_size(tree->left);
+		if (tree->right)
+			count += binary_tree_size(tree->right);
+		count++;
 	}
-	return (left == right);
+	return (count);
 }
 
 /**
@@ -64,59 +107,13 @@ heap_t *heap_insert(heap_t **root, int value)
 		(*root) = new_node;
 		return (new_node);
 	}
-	if (value > (*root)->n)
-	{
-		new_node = (heap_t *)binary_tree_node(NULL, value);
-		if (!new_node)
-			return (NULL);
-		new_node->parent = (*root)->parent;
-		(*root)->parent = new_node;
-		if (!(*root)->left)
-		{
-			new_node->left = (*root);
-			new_node->right = (*root)->right;
-			(*root)->right = NULL;
-			if (new_node->right)
-				new_node->right->parent = new_node;
-		}
-		else if (!(*root)->right)
-		{
-			new_node->left = (*root)->left;
-			new_node->right = (*root);
-			new_node->left->parent = new_node;
-			(*root)->left = NULL;
-		}
-		else if (!is_balanced_heap((*root)->right) &&
-				 is_balanced_heap((*root)->left))
-		{
-			new_node->left = (*root)->left;
-			new_node->left->parent = new_node;
-			new_node->right = (*root)->right;
-			new_node->right->parent = new_node;
-			value = (*root)->n;
-			free(*root);
-			heap_insert(&(new_node->right), value);
-		}
-		else
-		{
-			new_node->left = (*root)->left;
-			new_node->left->parent = new_node;
-			new_node->right = (*root)->right;
-			new_node->right->parent = new_node;
-			value = (*root)->n;
-			free(*root);
-			heap_insert(&(new_node->left), value);
-		}
-		(*root) = new_node;
-		return (new_node);
-	}
 	if (!(*root)->left)
 	{
 		new_node = (heap_t *)binary_tree_node((*root), value);
 		if (!new_node)
 			return (NULL);
 		(*root)->left = new_node;
-		return (new_node);
+		return (heapify(new_node));
 	}
 	if (!(*root)->right)
 	{
@@ -124,11 +121,18 @@ heap_t *heap_insert(heap_t **root, int value)
 		if (!new_node)
 			return (NULL);
 		(*root)->right = new_node;
-		return (new_node);
+		return (heapify(new_node));
 	}
-	if (!is_balanced_heap((*root)) && is_balanced_heap((*root)->left))
+	if (binary_tree_height((*root)->left) > binary_tree_height((*root)->right))
+	{
+		if (binary_tree_size((*root)->left) % 2 == 0 ||
+			binary_tree_balance((*root)->left) != 0)
+			return (heap_insert(&(*root)->left, value));
+		else
+			return (heap_insert(&(*root)->right, value));
+	}
+	if (binary_tree_size((*root)->left) > binary_tree_size((*root)->right))
 		return (heap_insert(&(*root)->right, value));
-	return (heap_insert(&(*root)->left, value));
-
-	return (NULL);
+	else
+		return (heap_insert(&(*root)->left, value));
 }
--- 0x0C-heap_insert/binary_trees.h
+++ 0x0C-heap_insert/binary_trees.h
@@ -23,8 +23,11 @@ typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+heap_t *heapify(heap_t *node);
 void binary_tree_print(const binary_tree_t *);
 
 size_t binary_tree_height(heap_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+int binary_tree_balance(heap_t *tree);
 int is_balanced_heap(heap_t *root);
 #endif /* BINARY_TREES */
