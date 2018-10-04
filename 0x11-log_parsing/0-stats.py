2b6921ff8de6836e803854f9ee7dcbdfc4b837c3
jarehec <129@holbertonschool.com>
2018-10-03 17:54:41 -0700

    initial commit

---
+++ 0x11-log_parsing/0-stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+parses a log file
+"""
+import fileinput
+import signal
+from collections import defaultdict
+
+valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
+total_size = int()
+counter = int()
+logs = defaultdict(int)
+
+
+def handler(signum, frame):
+    """ signal handler """
+    print('File size:', total_size)
+    for i in sorted(logs.keys()):
+        print('{}: {}'.format(i, logs[i]))
+    counter = 0
+
+signal.signal(signal.SIGINT, handler)
+
+for line in fileinput.input():
+    line = line.split(' ')
+    if len(line) == 9:
+        total_size += int(line[8])
+        if len(valid_codes.intersection({line[7]})) == 1:
+            logs[line[7]] += 1
+    if counter == 9:
+        print('File size:', total_size)
+        for i in sorted(logs.keys()):
+            print('{}: {}'.format(i, logs[i]))
+        counter = 0
+    counter += 1
---
+++ 0x11-log_parsing/README.md
@@ -0,0 +1,8 @@
+# Log parsing
+
+## Synopsis
+This script parses a log file with the format `<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>`. Outputs the file size, and status codes every 10 seconds.
+
+
+## Authors
+* Jared Heck
