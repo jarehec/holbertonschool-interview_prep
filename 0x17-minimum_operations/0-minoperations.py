0d41eb275ae8566ae4d2807a5fea42c5caf590c6
jarehec <129@holbertonschool.com>
2018-11-16 19:45:25 -0800

    fixed :)

--- 0x17-minimum_operations/0-minoperations.py
+++ 0x17-minimum_operations/0-minoperations.py
@@ -4,16 +4,14 @@ modulue containing minOperations and helper functions
 """
 
 
-def gcd(n: int):
-    """ returns the greatwst divisor into n """
-    d = 1
-    for i in range(d, n // 2 + 1):
+def gcd(n):
+    """ returns the greatest divisor into n """
+    for i in range(n // 2, 0, -1):
         if n % i == 0:
-            d = i
-    return d
+            return i
 
 
-def get_divisors(n: int):
+def get_divisors(n):
     """ returns all the divisors into n """
     l = [n]
     while gcd(n) != 1:
@@ -24,20 +22,20 @@ def get_divisors(n: int):
 
 def minOperations(n):
     """ calculates the minimum operations to reach n characters in a file """
+    if n <= 1:
+        return 0
     divisors = get_divisors(n)
     if len(divisors) == 1:
         return n
+
     ops = 0
     offset = -1
     size = 1
+
     while size != n:
-        if divisors[offset] // 2 == size:
-            ops += 2
-            size *= 2
-            offset = offset - 1 if -len(divisors) < offset else offset
-        elif size*size == divisors[offset]:
-            ops += size
-            size *= size
+        if divisors[offset] % size == 0:
+            ops += divisors[offset] // size
+            size *= divisors[offset] // size
             offset = offset - 1 if -len(divisors) < offset else offset
         else:
             ops += 1
