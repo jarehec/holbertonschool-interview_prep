01de905e8e2f6df618f3136c6214b96141b08cb4
jarehec <bone1198@gmail.com>
2018-11-24 18:46:28 -0800

    *shrug*

--- 0x18-utf8_validation/0-validate_utf8.py
+++ 0x18-utf8_validation/0-validate_utf8.py
@@ -4,16 +4,10 @@ Module containing utf validator
 """
 
 
-def validUTF8(bytes_list=[]):
-    for i in bytes_list:
-        if not chr(i).isprintable():
-            return False
-        elif i >= 0x0000 and i <= 0x007F:
-            pass
-        elif i >= 0x0080 and i <= 0x07FF:
-            pass
-        elif i >= 0x0800 and i <= 0x10FFFF:
-            pass
-        else:
-            return False
-    return True
+def validUTF8(data):
+    """ validates utf8 bytes """
+    try:
+        bytes([i for i in data])
+        return True
+    except:
+        return False
