f5f6fc34d97436268012aaa09fec2ceb4ad901d0
jarehec <bone1198@gmail.com>
2018-11-26 22:16:04 -0800

    maybe

--- 0x18-utf8_validation/0-validate_utf8.py
+++ 0x18-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,28 @@ Module containing utf validator
 """
 
 
+def to_bin(data):
+    """ gives the 8 bit binary representation """
+    return [bin(i)[2:].zfill(8) for i in data]
+
+
 def validUTF8(data):
     """ validates utf8 bytes """
-    try:
-        bytes([i for i in data])
+    bin_data = to_bin(data)
+    if data[0] <= 127 and data[0] >= 0:
+        for i in data:
+            if i < 0 or i > 127:
+                return False
         return True
-    except:
+
+    n_bytes = 0
+    for i in bin_data[0]:
+        if i == '0':
+            break
+        n_bytes += 1
+    if n_bytes != len(data):
         return False
+    for i in bin_data[1:]:
+        if i[:2] != '10':
+            return False
+    return True
