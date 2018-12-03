794e9711aaaf308ef26c5f5af350ff4b1c6ae493
jarehec <bone1198@gmail.com>
2018-12-02 19:30:30 -0800

    initial commit

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module containing makeChange
+"""
+
+def coinChange(coins, total):
+    """ calculates the minimum amount of coins to meet a given amount """
+    if total < 1:
+        return 0
+    amount, i = int(), -1
+    coins.sort()
+    while total > 0 and i > -len(coins):
+        amount += (total // coins[i])
+        if total % coins[i] == 0:
+            total -= (total // coins[i]) * coins[i]
+        elif total > coins[i]:
+            total -= (total // coins[i]) * coins[i]
+        i -= 1
+    return amount if total == 0 else -1
---
+++ 0x19-making_change/README.md
@@ -0,0 +1,6 @@
+# Making change
+
+This function calculates the minimum amount of coins to meet a given amount 
+
+## Authors
+* Jared Heck
