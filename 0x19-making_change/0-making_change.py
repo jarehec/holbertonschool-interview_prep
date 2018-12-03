522c204f68a95bc68dce239bbd54ed894a7b0578
jarehec <bone1198@gmail.com>
2018-12-02 19:32:49 -0800

    pep8

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,6 +3,7 @@
 module containing makeChange
 """
 
+
 def coinChange(coins, total):
     """ calculates the minimum amount of coins to meet a given amount """
     if total < 1:
@@ -10,10 +11,11 @@ def coinChange(coins, total):
     amount, i = int(), -1
     coins.sort()
     while total > 0 and i > -len(coins):
-        amount += (total // coins[i])
         if total % coins[i] == 0:
+            amount += (total // coins[i])
             total -= (total // coins[i]) * coins[i]
         elif total > coins[i]:
+            amount += (total // coins[i])
             total -= (total // coins[i]) * coins[i]
         i -= 1
     return amount if total == 0 else -1
