df26491101dabe09f7d1f8182058f267129cf9d8
jarehec <bone1198@gmail.com>
2019-01-20 23:58:11 -0800

    initial commit

---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,8 @@
+#ifndef WILD_CMP
+#define WILD_CMP
+
+int str_len(char *str, int len);
+int walk(char *s1, char *s2, int *offset);
+int wildcmp(char *s1, char *s2);
+
+#endif /* WILD_CMP */
---
+++ 0x1E_wild_cmp/README.md
@@ -0,0 +1,8 @@
+# Wild Compare
+
+## Synopsis
+function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.
+
+
+## Authors
+* Jared Heck
